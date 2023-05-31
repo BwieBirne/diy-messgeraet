5dc4698b82cf3f1ace5b062f8074863a73c0f18a
BwieBirne <102586857+BwieBirne@users.noreply.github.com>
2023-05-31 08:50:10 +0200

    👍👍👍😎😎😎

--- diy-messgeraet-ino/diy-messgeraet-ino.ino
+++ diy-messgeraet-ino/diy-messgeraet-ino.ino
@@ -1,22 +1,50 @@
 
+//enums
+enum frequency_type { AC,
+                      DC,
+                      DAC };
+enum measurement_type { U,
+                        I };
+
+//pins
+#define U_PIN A0
+#define I_PIN A3
+
+//constants
+#define MEASUREMENT_ITR 100
+#define MEASUREMENT_DELAY 20
+
+#define U_DIVIDER 40.6f
+#define I_DIVIDER 40.0f
+#define I_OFFSET 508
+
+#define SQRT2 1.4142
+
+//varibales
+enum frequency_type f_type = DC;
+enum measurement_type m_type = U;
+float current_U = 0.0f;
+float current_I = 0.0f;
+
+//timer
+#define MEASUREMENT_INTERVAL 1000
+long int m_timer = 0;
+
 void setup() {
 
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(A3, INPUT);
+  pinMode(U_PIN, INPUT);
+  pinMode(I_PIN, INPUT);
 
+  Serial.println("A");
 }
 
 void loop() {
 
-  Serial.println((508-analogRead(A0))/40.0f);
   timer();
-
 }
 
-void timer(){
+void timer() {
 
-  measurement(A0);
-
+  measurement(U_PIN);
 }
-
--- diy-messgeraet-ino/measurement.ino
+++ diy-messgeraet-ino/measurement.ino
@@ -1,12 +1,58 @@
 
-float measurement(const int mPin){
+float measurement(const int mPin) {
 
+
+  //max gibt Spitzenwert
+  //min gibt DC Offset
+  //avg ist NICHT der Effektivwert!!
   int mArray[MEASUREMENT_ITR];
+  long int avg = 0;
+  int min = 1023;
+  int max = -1023;
 
   for (int i = 0; i < MEASUREMENT_ITR; i++) {
 
     mArray[i] = analogRead(mPin);
-
+    delayMicroseconds(MEASUREMENT_DELAY);
   }
 
+  for (int i = 0; i < MEASUREMENT_ITR; i++) {
+    if (mArray[i] < min) min = mArray[i];
+    if (mArray[i] > max) max = mArray[i];
+    avg += mArray[i];
+  }
+
+  if (m_type == I) {
+    for (int i = 0; i < MEASUREMENT_ITR; i++) {
+      mArray[i] -= I_OFFSET;
+    }
+    min -= I_OFFSET;
+    max -= I_OFFSET;
+    avg = avg - (MEASUREMENT_ITR * I_OFFSET);
+  }
+
+  avg = avg / MEASUREMENT_ITR;
+
+  if (max - min > avg) {
+    //AC kann nur über I festgestellt werden!
+    f_type = DAC;
+  } else {
+    f_type = DC;
+  }
+
+  switch (f_type) {
+    case DC:
+      avg = (min + ((max - min) / (SQRT2)));
+    case DAC:
+      avg = (min + ((max - min) / (SQRT2)));
+    case AC:
+      avg = (min + ((max - min) / (SQRT2)));
+  }
+
+  switch (m_type) {
+    case U:
+      return (avg / U_DIVIDER);
+    case I:
+      return ((I_OFFSET - avg) / I_DIVIDER);
+  }
 }
