b31d4843b4e121a8bf2b6aef1c67eed99057dcd6
BwieBirne <102586857+BwieBirne@users.noreply.github.com>
2023-06-14 08:22:45 +0200

    Update communication.ino

--- diy-messgeraet-ino/communication.ino
+++ diy-messgeraet-ino/communication.ino
@@ -4,5 +4,18 @@
 
 void serialEvent() {
 
+  while(Serial.available()) {
+
+    while(Serial.peek() == 10 or Serial.peek() == 13) {
+      Serial.read();
+    }
+
+    int statusInt = Serial.parseInt();
+
+    if (statusInt == 100) {
+      
+    }
+
+  }
 
 }
